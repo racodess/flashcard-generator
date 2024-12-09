7580e94b3d0a264c9e52f0afce1e91133f1205dc
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-12-09 13:54:00 -0500

    Refactor code with helper methods

--- generator.py
+++ generator.py
@@ -2,29 +2,24 @@ import os
 import sys
 import io
 import base64
-import json
 
-from pdf2image import convert_from_path
+from pdfminer.high_level import extract_text
 from PIL import Image
 from rich import print_json
-from rich.text import Text
 from rich.pretty import Pretty
 from rich.console import Console
 from rich.markdown import Markdown
 from openai import OpenAI
 
 from prompts import (
-    TEXT,
+    TEXT_FORMAT,
     FLASHCARD_SCHEMA,
-    SYSTEM_MESSAGE,
-    INITIAL_PROMPT_IMG,
-    INITIAL_PROMPT_TEXT,
-    FRONT_BACK_PROMPT,
-    WORDINESS_PROMPT,
-    FIND_EXAMPLES_PROMPT,
-    ADD_EXAMPLES_PROMPT,
-    CITATIONS_PROMPT,
-    REMOVE_BAD_QUESTIONS_PROMPT,
+    PLACEHOLDER_MESSAGE,
+    REWRITE_PROMPT,
+    BRAINSTORM_KNOWLEDGE_MAP_PROMPT,
+    IMAGE_ANALYSIS_PROMPT,
+    BRAINSTORM_FLASHCARDS_PROMPT,
+    JSON_FLASHCARDS_PROMPT,
 )
 
 # OpenAI Models
@@ -32,13 +27,8 @@ GPT_O1_PREVIEW = "o1-preview"
 GPT_O1_MINI = "o1-mini"
 GPT_4O = "gpt-4o"
 GPT_4O_MINI = "gpt-4o-mini"
-
-# Use this model
-MODEL = GPT_4O_MINI
-
 # Initialize the OpenAI client
 client = OpenAI()
-
 # Initialize Rich console for formatted output
 console = Console()
 
@@ -47,7 +37,9 @@ def detect_file_type(file_path):
     _, ext = os.path.splitext(file_path)
     if ext.lower() == '.txt':
         return 'text'
-    elif ext.lower() in ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.pdf']:
+    elif ext.lower() == '.pdf':
+        return 'pdf'
+    elif ext.lower() in ['.png', '.jpg', '.jpeg', '.gif', '.bmp']:
         return 'image'
     else:
         return 'unsupported'
@@ -56,29 +48,26 @@ def detect_file_type(file_path):
 def process_file(file_path, file_type):
     if file_type == 'text':
         with open(file_path, 'r', encoding='utf-8') as f:
-            content = f.read()
-        initial_prompt = INITIAL_PROMPT_TEXT.format(TEXT=content)
-        img_uri = None
+            plain_text = f.read()
+        return plain_text
     elif file_type == 'image':
-        if file_path.lower().endswith('.pdf'):
-            images = convert_doc_to_images(file_path)
-            img_uri = get_img_uri(images[0])
-        else:
-            img = Image.open(file_path)
-            img_uri = get_img_uri(img)
-        initial_prompt = INITIAL_PROMPT_IMG
+        img = Image.open(file_path)
+        img_uri = get_img_uri(img)
+        return img_uri
+    elif file_type == 'pdf':
+        pdf_text = extract_text_from_doc(file_path)
+        return pdf_text
     else:
         console.print(
             f"Unsupported file type: '{file_path}'.\n"
-            "Supported file types are '.txt', '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.pdf'",
+            "Supported file types are '.txt', '.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp'",
             style="red"
         )
         sys.exit(1)
-    return initial_prompt, img_uri
 
 
-def convert_doc_to_images(path):
-    return convert_from_path(path)
+def extract_text_from_doc(path):
+    return extract_text(path)
 
 
 def get_img_uri(img):
@@ -89,77 +78,104 @@ def get_img_uri(img):
     return f"data:image/png;base64,{base64_png}"
 
 
-def analyze_image(img_uri, message_list):
-    messages = message_list.copy()
-    messages.append(
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {"url": img_uri}
-                }
-            ]
-        }
-    )
-
-    try:
-        completion = client.chat.completions.create(
-            model=GPT_4O,
-            messages=messages,
-            max_tokens=1000,
-            temperature=0,
-            top_p=0.1
-        )
-        return completion
-    except Exception as e:
-        console.print(f"Error while analyzing image: {e}", style="red")
-        sys.exit(1)
-
-
-def get_completion(model, prompt, response_format, message_list):
-
+def get_completion(file_type, model, message_list, response_format):
     try:
         completion = client.chat.completions.create(
             model=model,
             messages=message_list,
             response_format=response_format,
-            max_tokens=16383,
+            max_completion_tokens=4000,
             temperature=0,
             top_p=0.1,
         )
+        finish_reason = completion.choices[0].finish_reason
+        if finish_reason != "stop":
+            console.print(
+                f"Finished processing {file_type} with reason: {finish_reason}",
+                style="yellow"
+            )
         return completion
     except Exception as e:
-        console.print(f"Error while generating response: {e}", style="red")
+        console.print(f"Error in processing {file_type}: ", Pretty(e), style="red")
         sys.exit(1)
 
 
-def print_user_message(message, markdown):
-    if markdown:
-        md = Markdown(message)
-        console.rule("[bold green]User Prompt")
-        console.print(f"\n\n", md, "\n")
-    else:
-        console.print(f"\n\n{message}\n")
 
-def print_model_response(model, response, markdown):
-    if markdown is True:
-        md = Markdown(response)
-        console.rule(f"[bold yellow]{model} Response")
-        console.print(f"\n\n", md, "\n")
+def handle_completion(file_type, system_message, response, file_contents, response_format):
+    message_list = [{"role": "system", "content": system_message}]
+
+    print_message("system", system_message, model=None, markdown=True)
+
+    if file_type.lower() == "image":
+        model = GPT_4O
+        content = [
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"{file_contents}"
+                }
+            }
+        ]
+        append_message("user", content, message_list)
+        print_message("user", PLACEHOLDER_MESSAGE, model=None, markdown=True)
+    elif system_message == REWRITE_PROMPT:
+        model = GPT_4O_MINI
+        append_message("user", file_contents, message_list)
+
+        print_message("text", file_contents, model=None, markdown=False)
     else:
-        console.rule(f"[bold yellow]{model} Response")
-        print_json(response)
+        model = GPT_4O_MINI
+        append_message("user", response, message_list)
+
+        print_message("user", response, model=None, markdown=True)
+
+
+    completion = get_completion(
+        file_type,
+        model=model,
+        message_list=message_list,
+        response_format=response_format,
+    )
+
+    print_token_usage(completion)
+    model = completion.model
+    response = completion.choices[0].message.content
+
+    if response_format == FLASHCARD_SCHEMA:
+        print_message("assistant", response, model, markdown=False)
+    else:
+        print_message("assistant", response, model, markdown=True)
+
+    return response
+
+
+def print_message(role, content, model, markdown=False):
+    role_styles = {
+        "system": "[bold cyan] System Message",
+        "user": "[bold green]User Prompt (Source Material)",
+        "assistant": f"[bold yellow]{model} Response",
+        "token": "[bold magenta]Token Usage",
+        "text": "[bold red]Text Extracted From PDF"
+    }
+    rule = role_styles.get(role.lower(), f"[bold]{role} Message")
+    console.rule(rule)
+    if markdown and role.lower() != "token":
+        console.print("\n", Markdown(content), "\n")
+    elif role.lower() == "token":
+        console.print("\n", Pretty(content), "\n")
+    elif role.lower() == "text":
+        console.print("\n", content, "\n")
+    else:
+        print_json(content)
+
+
+def append_message(role, response, messages):
+    message = {"role": role.lower(), "content": response}
+    messages.append(message)
+
 
 def print_token_usage(completion):
-    console.rule(f"[bold magenta]Token Usage")
-    console.print(f"\n", Pretty(completion.usage), "\n")
-
-def append_user_prompt(prompt_text, message_list):
-    message_list.append({"role": "user", "content": prompt_text})
-
-def append_model_response(response, message_list):
-    message_list.append({"role": "assistant", "content": response})
+    print_message("token", completion.usage, model=None, markdown=False)
 
 
 def main():
@@ -169,35 +185,19 @@ def main():
 
     file_path = sys.argv[1]
     file_type = detect_file_type(file_path)
-    initial_prompt, img_uri = process_file(file_path, file_type)
-
-    message_list = [SYSTEM_MESSAGE]
+    file_contents = process_file(file_path, file_type)
 
     prompts = [
-        ("Initial Prompt", initial_prompt, TEXT),
-        ("Front and Back Prompt", FRONT_BACK_PROMPT, FLASHCARD_SCHEMA),
-        ("Wordiness Prompt", WORDINESS_PROMPT, FLASHCARD_SCHEMA),
-        ("Find Examples Prompt", FIND_EXAMPLES_PROMPT, TEXT),
-        ("Add Examples Prompt", ADD_EXAMPLES_PROMPT, FLASHCARD_SCHEMA),
-        ("Citations Prompt", CITATIONS_PROMPT, FLASHCARD_SCHEMA),
-        ("Remove Bad Questions Prompt", REMOVE_BAD_QUESTIONS_PROMPT, FLASHCARD_SCHEMA),
+        (REWRITE_PROMPT, TEXT_FORMAT),
+        (BRAINSTORM_KNOWLEDGE_MAP_PROMPT, TEXT_FORMAT),
     ]
 
-    for title, prompt_text, response_format in prompts:
-        print_user_message(prompt_text, markdown=True)
-        if title == "Initial Prompt" and initial_prompt == INITIAL_PROMPT_IMG:
-            completion = analyze_image(img_uri, message_list)
-            append_user_prompt("See assistant's presentation below:", message_list)
-        else:
-            append_user_prompt(prompt_text, message_list)
-            completion = get_completion(MODEL, prompt_text, response_format, message_list)
-        print_token_usage(completion)
-        response = completion.choices[0].message.content
-        append_model_response(response, message_list)
-        if response_format == FLASHCARD_SCHEMA:
-            print_model_response(MODEL, response, markdown=False)
-        else:
-            print_model_response(MODEL, response, markdown=True)
+    if file_type.lower() == "image":
+        prompts.insert(0, (IMAGE_ANALYSIS_PROMPT, TEXT_FORMAT))
+
+    response = None
+    for system_message, response_format in prompts:
+        response = handle_completion(file_type, system_message, response, file_contents, response_format)
 
 
 if __name__ == "__main__":
