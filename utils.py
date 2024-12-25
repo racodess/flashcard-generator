628e4e86a2c51c20a47ac074d5c7147ce03bcd2e
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-12-25 15:54:10 -0500

    Refactor: move more utility methods into utils

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 import io
 import sys
@@ -7,6 +8,8 @@ import argparse
 from PIL import Image
 from pdf2image import convert_from_path
 from pdfminer.high_level import extract_text
+from rich.markdown import Markdown
+from rich.pretty import pprint
 
 from weasyprint import HTML, CSS
 import markdown2
@@ -130,3 +133,56 @@ def create_pdf_from_markdown(collection_media_path, file_name, text_markdown):
         print(f"PDF created successfully at: {pdf_path}")
     except Exception as e:
         print(f"Failed to create PDF from txt file: {e}")
+
+
+def parse_llm_response(response_str):
+    try:
+        data = json.loads(response_str)
+
+        concepts = data.get("concepts", [])
+
+        return concepts
+
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON in parse_llm_response method: {e}")
+
+        return [], []
+
+
+def print_message(role, content, response_format, model, markdown):
+    role_styles = {
+        "system": "[bold cyan]System Message",
+        "user": "[bold green]User Message",
+        "assistant": f"[bold yellow]{model}: Response",
+        "token": "[bold magenta]Token Usage",
+        "flashcard": "[bold orange]Flashcards",
+    }
+
+    rule = role_styles.get(role.lower(), f"[bold]{role} Message")
+    console.rule(rule)
+
+    if markdown and isinstance(content, str):
+        console.print("\n", Markdown(content), "\n")
+        return
+
+    if role.lower() == "token":
+        print("\n")
+        pprint(content, expand_all=True)
+        print("\n")
+        return
+
+    if role.lower() == "assistant":
+        text_content = response_format.model_validate_json(content)
+        pprint(text_content)
+        return
+
+    if role.lower() == "flashcard":
+        pprint(content)
+        return
+
+    console.print("\n", content, "\n")
+
+
+def append_message(role, response, messages):
+    message = {"role": role.lower(), "content": response}
+    messages.append(message)
